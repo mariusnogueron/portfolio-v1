1bedf7e20e5d54583e089f4e3d33e7f1068d0d41
mariusnogueron <marius.nogueron@gmail.com>
2024-03-25 14:29:56 +0100

    test carrousel 5

--- script/script.js
+++ script/script.js
@@ -1,10 +1,6 @@
 const lenis = new Lenis();
 let scrollSpeed = 1; 
 
-lenis.on('scroll', (e) => {
-   console.log(e);
-});
-
 function raf(time) {
    lenis.raf(time * scrollSpeed); 
    requestAnimationFrame(raf);
